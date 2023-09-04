bd82964ab26457b4bf3c58b4eabb55dce9a6f12b
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2023-09-04 17:36:07 -0300

    script

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function termOperator(){
 function populationVector(){
   let exit_loop = false;
   let n_loop;
+  
   do{
     try {
       n_loop = prompt("Digite a quantidade de operações a serem calculadas: ");
@@ -39,7 +40,32 @@ function populationVector(){
     }
   }while(exit_loop === false);
   
-  for(i=0; i < n_loop; i++){
+  let vector = new Array(n_loop);
+
+  for(let i=0; i < n_loop; i++){
+    let temp = prompt(`Digite o valor do ${i+1}:`);
+    // let x, y;
     
+    // try{
+    //   x = parseInt(temp);
+    //   y = parseFloat(temp);  
+    // }catch(erorr){
+    //   alert("O seguinte erro foi encontrado: " + error);
+    // }
+
+    if(Number.parseInt(temp) === NaN || Number.parseFloat(temp) === NaN 
+    || temp.includes(',') === true || temp === undefined || temp === ""){
+      i--;
+      alert('Por favor digite numeros inteiros ou numros quebrados com o ponto ex: "3.2"');
+    }else{
+      vector[i] = Number.parseFloat(temp);
+    }  
   }
+  console.log(vector);
+}
+
+
+function teste(){
+  let valor = prompt("Digite um numero:");
+  console.log(valor.includes(','));
 }
